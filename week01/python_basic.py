69ba0dc67d59676096484e46868d15592068ac4a
조성빈 <choys4444@gmail.com>
2026-03-15 20:26:36 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -6,4 +6,19 @@ print(a ** b)   # 81
 print(a ** 3)   # 27
 
 # 나머지 연산
-print(a % b)    # 
+print(a % b)    # 3
+print(7 % 3)    # 1
+
+# 나눗셈 몫 구하기
+print(a // b)   # 0
+print(7 // 3)   # 2
+
+s1 = 'Hello Python'
+print(s1)
+
+s3 = '''Hello
+Python'''
+print(s3)
+
+head = "Python"
+tail = " is fun"
