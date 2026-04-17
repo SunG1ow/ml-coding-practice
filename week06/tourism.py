a1b8f6378c1696cbdefd217dbff138556546e678
조성빈 <choys4444@gmail.com>
2026-04-17 23:04:32 +0900

    Updated Changes

--- week06/tourism.py
+++ week06/tourism.py
@@ -8,3 +8,20 @@ SerciveKey = "3c9b5c5bbb8752d28c8e400504f7fe632c1d0e9d0f0cdaf570a48b5c62aaafe9"
 
 """### [CODE 0]"""
 
+
+
+"""### [CODE 1]"""
+
+def getRequestUrl(url):
+    req = urllib.request.Request(url)
+    try:
+        response = urllib.request.rulopen(req)
+        if response.getcode() == 200:
+            print("[%s] Url Request Success" % datetime.datetime.now())
+            return response.read().decode('utf-8')
+    except Exception as e:
+        print(e)
+        print("[%s] Error for URL : %s" % (datetime.datetime.now(), url))
+        return None
+    
+main()
