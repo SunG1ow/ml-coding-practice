cc9a4f2cd59f1431d625192ea683060f2fc16f80
조성빈 <choys4444@gmail.com>
2026-04-17 22:39:31 +0900

    Updated Changes

--- week06/naveraip.py
+++ week06/naveraip.py
@@ -15,4 +15,16 @@ def main():
     jsonResult = []
 
     jsonResponse = getNaverSearch(node, srcText, 1, 100)
+
+
+
+
+
+
+def getNaverSearch(node, srcText, page_start, display):
+    base = "https://openapi.naver.com/v1/search"
+    node = "/%s.json" % node
+    parameters = "?query=%s&start=%s&display=%s" % (urllib.parse.quote(srcText), page_start, display)
     
+    url = base + node + parameters
+    responseDecode = getRequestUrl()
