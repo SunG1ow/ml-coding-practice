dbd032c88c52a4fe518370323bf8435073575af8
조성빈 <choys4444@gmail.com>
2026-05-05 23:30:17 +0900

    Updated Changes

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -141,3 +141,14 @@ plt.grid(axis='x')
 plt.show()
 
 netflix[netflix['title'].str.contains('Sankofa', na=False, case=False)]
+
+# 출력할 최대 행 수를 None으로 설정해서 모두 출력
+pd.set_option('display.max_rows', None)
+
+# 쉽표로 country 열의 값을 파이썬 리스트로 만들기
+netflix['country'] = netflix['country'].str.split(', ')
+netflix['country']
+
+# 파이썬 리스트로 바꾼 country 열의 값에 explode() 함수를 적용하여 개별 행으로 분리
+netflix_age_country = netflix.explode('country')
+netflix_age_country
