f936d24b32d5eb103b471864290c006eb91098f7
조성빈 <choys4444@gmail.com>
2026-05-05 21:29:00 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -35,3 +35,9 @@ plt.show()
 X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
 
 # 이진 분류기 훈련
+y_train_5 = (y_train == '5')    # 5는 true고, 다른 숫자는 모두 false
+y_test_5 = (y_test == '5')
+
+from sklearn.linear_model import SGDClassifier
+
+sgd_clf = 
