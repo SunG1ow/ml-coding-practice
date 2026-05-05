1b66075bb51118156e23e3f6544b1093cf2046ca
조성빈 <choys4444@gmail.com>
2026-05-05 21:44:01 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -88,3 +88,10 @@ from sklearn.metrics import roc_auc_score
 y_scores = cross_val_predict(sgd_clf, X_train, y_train_5, cv=3, method="decision_function")
 roc_auc_score(y_train_5, y_scores)
 
+from sklearn.ensemble import RandomForestClassifier
+
+forest_clf = RandomForestClassifier(random_state=42)
+
+y_probas_forest = cross_val_predict(forest_clf, X_train, y_train_5, cv=3, method="predict_proba")
+
+y_scores_forest = 
